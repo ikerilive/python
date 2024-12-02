ddcc0f764ea4efd7ff3f88fa535e2ad0d9efbe05
AF2228 <AF2228@student.jamk.fi>
2024-12-02 15:26:18 +0200

    Added project to repository

--- oob-python/NATO-alphabet-naming/main.py
+++ oob-python/NATO-alphabet-naming/main.py
@@ -7,8 +7,16 @@ phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
+while True:
+    try:
+        word = input("Enter a word: ").upper()
 
-output_list = [phonetic_dict[letter] for letter in word]
+        # Check if the word contains only alphabets
+        if not word.isalpha():
+            raise ValueError("Please, enter only alphabets.")
 
-print(output_list)
+        output_list = [phonetic_dict[letter] for letter in word]
+
+        print(output_list)
+    except (KeyError, ValueError) as e:
+        print(e)
